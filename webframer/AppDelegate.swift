1b183d236cd5fc638651c958a50505da4c877016
Joseph Price <joseph.price@huffingtonpost.com>
2015-06-10 00:39:22 -0400

    Initial Commit

---
+++ webframer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  webframer
+//
+//  Created by Price, Joseph on 6/10/15.
+//  Copyright (c) 2015 Price, Joseph. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ webframer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  webframer
+//
+//  Created by Price, Joseph on 6/10/15.
+//  Copyright (c) 2015 Price, Joseph. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
